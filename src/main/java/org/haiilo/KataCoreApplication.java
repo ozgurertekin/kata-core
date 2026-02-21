3aa27fe8aaeddc521ad8da1b3d48d5164e2e53cf
ozgurertekin <49617826+ozgurertekin@users.noreply.github.com>
2026-02-21 09:55:30 +0100

    Intial commit

---
+++ src/main/java/org/haiilo/KataCoreApplication.java
@@ -0,0 +1,11 @@
+package org.haiilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KataCoreApplication.class, args);
+    }
+}
